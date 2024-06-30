834114b76fa8c531766b0dbd59c2e6c92a8ae5b7
SangWon7242 <gotkdqja@gmail.com>
2024-06-30 11:50:05 +0900

    박스 안의 글자 넣기

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: Container(
-        child: Text('박스안의 글자'),
-        color: Colors.blue,
-      ),
+      home: Center(
+          child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              child: Text(
+                  '박스안의 글자',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.none,
+                ),
+              )
+          ),
+      )
     );
   }
 }
